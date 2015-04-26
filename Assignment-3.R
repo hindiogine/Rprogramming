f453820c38248313499999e796bb9acd9d6c56af
S. Enrico P. Indiogine <hindiogine@gmail.com>
2015-04-26 15:26:26 -0600

    quasi finito seconda funzione

--- Assignment-3.R
+++ Assignment-3.R
@@ -4,17 +4,16 @@ best <- function(state, outcome)
 {
   ## Read outcome data
   temp <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings = "Not Available")
-  hosp.data <- temp[,c(2,7,11,17,23)]
+  hosp.data <- na.omit(temp[,c(2,7,11,17,23)])
   names(hosp.data) <- c("Name","State","Heart.attack","Heart.failure","Pneumonia")
   hosp.data$Heart.attack  <- as.numeric(hosp.data$Heart.attack)
   hosp.data$Heart.failure <- as.numeric(hosp.data$Heart.failure)
   hosp.data$Pneumonia     <- as.numeric(hosp.data$Pneumonia)
-  
   states <- unique(hosp.data$State)
+  
   ## Check that state and outcome are valid
   temp <- grepl(state, states)
   if(sum(temp)!=1) stop("invalid state")
-  
   valid.outcomes <- c("heart attack","heart failure","pneumonia")
   temp <- grepl(outcome, valid.outcomes)
   if(sum(temp)!=1) stop("invalid outcome")
@@ -26,19 +25,44 @@ best <- function(state, outcome)
   
   ## rate of "heart attack", "heart failure", "pneumonia".
   hosp.data <- hosp.data[hosp.data$State == state, c(1, outcome.column)]
-  min(hosp.data[,2], na.rm = TRUE)
-  
-  best.hosp.row <- which(hosp.data[,2] == min(hosp.data[,2], na.rm = TRUE))
-  
-  hosp.data[best.hosp.row, 1]
+  best.hosp.rows <- which(hosp.data[,2] == min(hosp.data[,2]))
+  best.hosp <- hosp.data[best.hosp.rows, 1]
+  best.hosp <- best.hosp[order(best.hosp$Name), ]
+  best.hosp[1, 1]
 }
 
 rankhospital <- function(state, outcome, num = "best")
 {
   ## Read outcome data
+  temp <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings = "Not Available")
+  hosp.data <- na.omit(temp[,c(2,7,11,17,23)])
+  names(hosp.data) <- c("Name","State","Heart.attack","Heart.failure","Pneumonia")
+  hosp.data$Heart.attack  <- as.numeric(hosp.data$Heart.attack)
+  hosp.data$Heart.failure <- as.numeric(hosp.data$Heart.failure)
+  hosp.data$Pneumonia     <- as.numeric(hosp.data$Pneumonia)
+  states <- unique(hosp.data$State)
+  
   ## Check that state and outcome are valid
+  temp <- grepl(state, states)
+  if(sum(temp)!=1) stop("invalid state")
+  valid.outcomes <- c("heart attack","heart failure","pneumonia")
+  temp <- grepl(outcome, valid.outcomes)
+  if(sum(temp)!=1) stop("invalid outcome")
+  
   ## Return hospital name in that state with the given rank
   ## 30-day death rate
+  if(outcome == "heart attack") outcome.column <- 3
+  if(outcome == "heart failure") outcome.column <- 4
+  if(outcome == "pneumonia") outcome.column <- 5
+  
+  hosp.data <- hosp.data[hosp.data$State == state, c(1, outcome.column)]  
+  hosp.data <- hosp.data[order(hosp.data[,2]), ]  
+  
+  rank <- num
+  if(num == "best") { rank <- 1 }
+  if(num == "worst") { rank <- nrow(hosp.data) }
+  ranked.hosp <- hosp.data[order(hosp.data[,1]), ]
+  ranked.hosp[rank, 1]
 }
 
 rankall <- function(outcome, num = "best")
