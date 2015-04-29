1ade526c7d357532635adfaab1fdd1e80fddad58
S. Enrico P. Indiogine <hindiogine@gmail.com>
2015-04-28 20:13:42 -0600

    Iniziato terza parte

--- Assignment-3.R
+++ Assignment-3.R
@@ -68,7 +68,21 @@ rankhospital <- function(state, outcome, num = "best")
 rankall <- function(outcome, num = "best")
 {
   ## Read outcome data
+  temp <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings = "Not Available")
+  hosp.data <- na.omit(temp[,c(2,7,11,17,23)])
+  names(hosp.data) <- c("Name","State","Heart.attack","Heart.failure","Pneumonia")
+  hosp.data$Heart.attack  <- as.numeric(hosp.data$Heart.attack)
+  hosp.data$Heart.failure <- as.numeric(hosp.data$Heart.failure)
+  hosp.data$Pneumonia     <- as.numeric(hosp.data$Pneumonia)
+  states <- unique(hosp.data$State)
+  
   ## Check that state and outcome are valid
+  temp <- grepl(state, states)
+  if(sum(temp)!=1) stop("invalid state")
+  valid.outcomes <- c("heart attack","heart failure","pneumonia")
+  temp <- grepl(outcome, valid.outcomes)
+  if(sum(temp)!=1) stop("invalid outcome")  
+  
   ## For each state, find the hospital of the given rank
   ## Return a data frame with the hospital names and the
   ## (abbreviated) state name
